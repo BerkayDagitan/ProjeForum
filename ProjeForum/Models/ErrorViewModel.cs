ac64c373ffc7f01a9ea7ed265547ef7512d422e9
Berkay Dağıtan <stackzyy@gmail.com>
2025-05-24 14:50:02 +0300

    Add project files.

---
+++ ProjeForum/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ProjeForum.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
